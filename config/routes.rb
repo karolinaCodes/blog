7329cd81ae404cb7cfe466280121ddb2d9db167c
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-27 14:47:51 -0500

    create a Comments controller and a comments nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :articles
+
+  # *This creates comments as a nested resource within articles.*
+  resources :articles do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
