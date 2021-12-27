b18f30560e7b63e5f64403d818aaca15710d90d0
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-27 14:36:57 -0500

    add destroy method and link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,33 +10,39 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-
+ 
   def edit
     @article = Article.find(params[:id])
   end
-   
+ 
   def create
     @article = Article.new(article_params)
-   
+ 
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
-
+ 
   def update
     @article = Article.find(params[:id])
-   
+ 
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
-   
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
   private
-  # tells Rails exactly which parameters are allowed into our controller actions
     def article_params
       params.require(:article).permit(:title, :text)
     end
